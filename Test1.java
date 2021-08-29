ca643c0695f326d0b5733e0f5db57e792eded6ae
Pranit Aher <86141506+pranit-aher@users.noreply.github.com>
2021-08-29 13:16:52 +0530

    Add files via upload

--- Test1.java
+++ Test1.java
@@ -1,21 +1,16 @@
-import java.util.Scanner;
 
+/**
+ * @author Pranit
+ * Array Testing
+ */
 public class Test1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc= new Scanner(System.in);
-		System.out.println("Enter any name ");
-		String n="";
-		int i=0;
-		n=sc.nextLine();
-		System.out.println("Enter any int ");
-		i=sc.nextInt();
+		int [] [] array2D = {{0,1,2}, {3,4,5,6}};
 		
-		System.out.println(" name = "+n);
-		System.out.println(" i = "+i);
-		
-		sc.close();
+		System.out.print(array2D[0].length +" ");
+		System.out.print(array2D[1].getClass().isArray() + " ");
+		System.out.print(array2D[0] [1]);
 
 	}
 
